f288f6fe7e7849bf5fd8c4391e27eff5f7a84af3
VetheonGames <ceo@pixelatedstudios.net>
2023-01-02 14:39:10 -0700

    Change init method to instead just set instance variables instead of running functional code Moved functional code from init method to setup_check method that now contains the logic for determining what directory the manager needs to be working with remove redundant class variables and replace them with instance variables Rebase error_handler to ErrorHandler to obey CamelCase Finish setting up the make dirs method

--- classes/managers/Directory-manager.rb
+++ classes/managers/Directory-manager.rb
@@ -7,25 +7,32 @@ class Directories
   require_relative '../../tests/Nginx-test'
   require_relative '../handlers/Error-handler'
   require_relative '../system/Logman/Logman'
+  attr_accessor :install_dir, :type
 
-  def initialize(type)
-    @@instdir = '/etc/Pixelated-Studios/NodeChecker'
-    @@type = case type
-             when 'port'
-               'port'
-             when 'nginx'
-               'nginx'
-             else
-               @@type = Error_handler.new('starting the directory manager', 'Error Code: 384')
-               puts @@type
-             end
+  def initialize
+    @install_dir = '/etc/Pixelated-Studios/NodeChecker'
+    @port_dir = '/etc/Pixelated-Studios/NodeChecker/tests/Port-test'
+    @nginx_dir = '/etc/Pixelated-Studios/NodeChecker/tests/Nginx-test'
+    @dirsreal = nil
+  end
+
+  def setup_check(dir_to_test)
+    @directory = case dir_to_test
+                 when 'port'
+                   @port_dir
+                 when 'nginx'
+                   @nginx_dir
+                 when 'install'
+                   @install_dir
+                 else
+                   ErrorHandler.new('fatal', 'System ran into issue starting the directory manager')
+                 end
   end
 
   def check
-    dirsreal = nil
-    dirsreal = 1 if Dir.exist?(@@instdir)
+    @dirsreal = 1 if Dir.exist?(@directory)
     # if dirsreal is 1, the install directory exists
-    case dirsreal
+    case @dirsreal
     when 1
       # so we run the dir_status with the 1 arg to ensure the dir isn't recreated
       dir_status(1)
@@ -40,19 +47,40 @@ class Directories
     when 1
       puts 'Working Directory Exists!'
     when 0
+      setup_make_dirs
+      stage_make_dirs
       make_dirs
     else
-      Error_handler.new('Checking if the Working Directory exists', 'Error Code: 591')
+      ErrorHandler.new('fatal', 'Checking the Working Directory status!')
+    end
+  end
+
+  def setup_make_dirs
+    puts "Working Directory Doesn't Exist!"
+    puts 'Create it now? (Y/N)'
+    @response = gets.chomp
+  end
+
+  def stage_make_dirs
+    case @response.downcase
+    when 'Y'
+      make_dirs
+    when 'N'
+      nil
+    else
+      ErrorHandler.new('fatal', 'System ran into Error creating Working Directory!')
     end
   end
 
   def make_dirs
-    mkdirs = system(mkdir, @@instdir)
-    case mkdirs
-    when true
-      puts 'Working Directory Created!'
-    else
-      Error_handler.new('while creating the working directory', 'Error Code: 662')
-    end
+    make_install_dir = system(mkdir, @install_dir)
+    make_install_dir_result = make_install_dir
+    Logman.log_to_stdout('info', 'Install Directory Created!') if make_install_dir_result
+    make_port_dir = system(mkdir, @port_dir)
+    make_port_dir_result = make_port_dir
+    Logman.log_to_stdout('info', 'Port Directory Created!') if make_port_dir_result
+    make_nginx_dir = system(mkdir, @nginx_dir)
+    make_nginx_dir_result = make_nginx_dir
+    Logman.log_to_stdout('info', 'Nginx Directory Created!') if make_nginx_dir_result
   end
 end
