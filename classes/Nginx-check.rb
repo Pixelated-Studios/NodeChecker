bcad76d12634fbb559dc73032cd32f969b4a5264
VetheonGames <ceo@pixelatedstudios.net>
2023-01-01 17:41:32 -0700

    Make the Nginx-checker use Logman

--- classes/Nginx-check.rb
+++ classes/Nginx-check.rb
@@ -4,16 +4,13 @@
 # class for checking if nginx is alive
 class Nginxcheck
   require_relative './Error-handler'
-  require_relative './Directory-manager'
   require_relative './Logman'
 
   def initialize
     if system('systemctl', 'is-active', '--quiet', 'nginx')
       puts 'Nginx Running!'
-      $nginxstatus = 0
     else
-      puts 'Nginx borked!'
-      $nginxstatus = 1
+      Error_handler.new('fatal', 'Nginx borked!')
     end
   end
 end
