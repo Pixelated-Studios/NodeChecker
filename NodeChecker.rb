adbd342ba217c02d9d18577e0f825457d6076277
VetheonGames <ceo@pixelatedstudios.net>
2023-01-02 14:04:25 -0700

    Refactor Port_check to PortCheck to obey CamelCase Refactor Error_handler to ErrorHandler to obey CamelCase

--- NodeChecker.rb
+++ NodeChecker.rb
@@ -13,7 +13,6 @@ Logman.setup_log_to_stdout
 
 two_lines = RubyFiglet::Figlet.new("Pixelated\nNodeCheck")
 two_lines.show
-Error_handler.new('info', 'Displayed Greeting')
 sleep(2)
 
 puts 'Checking NGINX status'
@@ -23,37 +22,37 @@ puts 'Would you like to test ports for this node? (Y/N)'
 resp1 = gets.chomp
 case resp1.downcase
 when 'Y'
-  Port_check.new
+  PortCheck.new
   puts 'Do you want to test Games or Systems ports? (G/S)'
   resp2 = gets.chomp
   case resp2.downcase
   when 'S'
-    case $nodenm
+    case @nodenm
     when 'dragon'
-      Port_check.port_type_dragon(1)
+      PortCheck.port_type_dragon(1)
     when 'gemini'
-      Port_check.port_type_gemini(1)
+      PortCheck.port_type_gemini(1)
     when 'shuttle'
-      Port_check.port_type_shuttle(1)
+      PortCheck.port_type_shuttle(1)
     else
-      Error_handler.new('fatal', 'Ran into issue running the Systems Port Check')
+      ErrorHandler.new('fatal', 'Ran into issue running the Systems Port Check')
     end
   when 'G'
-    case $nodenm
+    case @nodenm
     when 'dragon'
-      Port_check.port_type_dragon(2)
+      PortCheck.port_type_dragon(2)
     when 'gemini'
-      Port_check.port_type_gemini(2)
+      PortCheck.port_type_gemini(2)
     when 'shuttle'
-      Port_check.port_type_shuttle(2)
+      PortCheck.port_type_shuttle(2)
     else
-      Error_handler.new('fatal', 'Failure running the Games Port Check')
+      ErrorHandler.new('fatal', 'Failure running the Games Port Check')
     end
   else
-    Error_handler.new('fatal', 'Failure running the Port Check!')
+    ErrorHandler.new('fatal', 'Failure running the Port Check!')
   end
 when 'N'
   puts 'Skipping!'
 else
-  Error_handler.new('fatal', 'Critical Failure interpreting user input! Please contact Veth/Connor!')
+  ErrorHandler.new('fatal', 'Critical Failure interpreting user input! Please contact Veth/Connor!')
 end
